f1aef4cb249c8e23e13a811a38ee3b8cd8dd39d7
Rikuta Furuta <rikuta@furutan.com>
2020-07-05 22:39:05 +0900

    fix: tiny things

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.layers import Lambda, LeakyReLU, Conv2D, Input, Layer, Dense, Reshape, GaussianNoise
+from tensorflow.keras.layers import Lambda, LeakyReLU, Conv2D, Input, Layer, Dense, Reshape, GaussianNoise, LayerNormalization, Multiply, Add
+import tensorflow.keras.backend as K
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.backend import int_shape, ones
 import numpy as np
@@ -10,6 +11,20 @@ import shutil
 import subprocess
 import json
 import cv2
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Activation, Flatten, Input, BatchNormalization, Reshape, UpSampling2D, PReLU, ReLU, LeakyReLU, Lambda, GlobalAveragePooling2D, LayerNormalization, Multiply, Add
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.utils import to_categorical, plot_model
+import tensorflow as tf
+
+
+from tqdm.notebook import tqdm
+from multiprocessing import Pool
+import random
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from timeout_decorator import timeout, TimeoutError
 
 
 def SubpixelConv2D(input_shape, scale=2):
@@ -105,7 +120,8 @@ def create(generator, likelihood=0.999, size=None):
             img = cv2.resize(img, size)
         img = cv2.bilateralFilter(img, 5, 35, 35)
         return img
-    
+  
+"""
 #https://github.com/penny4860/keras-adain-style-transfer
 class AdaIN(Layer):
     def __init__(self, alpha=1.0, **kwargs):
@@ -138,8 +154,66 @@ class AdaIN(Layer):
         normalized_content_features = self.alpha * normalized_content_features + (1 - self.alpha) * content_features
         return normalized_content_features
 
-
+"""
 def parseshape(c):
     shape=int_shape(c)[1::]
     return shape
 
+
+def AdaINBlock(x,z):
+    x = LayerNormalization(axis=[1,2])(x)
+    shape = parseshape(x)
+    assert len(shape)==3
+    ch = shape[2]
+    ysi = Dense(ch)(z)
+    ysi = Reshape((1,1,ch))(ysi)
+    ysi = Lambda(K.tile, arguments={'n':(1,shape[0],shape[1],1)})(ysi)
+    ybi = Dense(ch)(z)
+    ybi = Reshape((1,1,ch))(ybi)
+    ybi = Lambda(K.tile, arguments={'n':(1,shape[0],shape[1],1)})(ybi)
+    x = Multiply()([x,ysi])
+    x = Add()([x,ybi])
+    return x
+
+def D_model_core(Height, Width, channel=3):
+    inputs = Input((Height, Width, channel))
+    x = Conv2D(64, (5, 5), padding="same", strides=(2,2))(inputs)
+    x = LeakyReLU(0.2)(x)
+    x = Conv2D(128, (5, 5), padding="same", strides=(2,2))(x)
+    x = LeakyReLU(0.2)(x)
+    x = Conv2D(256, (5, 5), padding="same", strides=(2,2))(x)
+    x = LeakyReLU(0.2)(x)
+    x = Flatten()(x)
+    x = Dropout(0.5) (x)
+    x = Dense(1024)(x)
+    x = LeakyReLU(0.2)(x)
+    x = Dropout(0.5)(x)
+    outputs = Dense(1, activation="sigmoid")(x)
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+def D_model(Height, Width, channel=3, n_estimators=3):
+    inputs = Input((Height, Width, channel))
+    outputs = []
+    for i in range(n_estimators):
+        outputs.append(D_model_core(Height, Width, channel)(inputs))
+    model = Model(inputs=inputs, outputs=outputs, name="discriminator")
+    return model
+
+def read_file(filepath,width,height):
+    img = cv2.imread(filepath)
+    if img is None:
+        print("no such file {0}".format(filepath))
+        return
+    _width, _height, _ = img.shape
+    if _width<256 or _height < 256:
+        return None
+    img = cv2.resize(img, (width,height))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+def read_file_wrap(dict):
+    filepath=dict["filepath"]
+    width=dict["width"]
+    height=dict["height"]
+    return read_file(filepath,width,height)
